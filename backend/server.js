224474e4e54ab688b45e58613c5ed7905d03580e
obarker <o.j.barker@hotmail.co.uk>
2021-04-01 00:52:22 +0100

    added product manager and API link to mongoDB.

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv'
 import config from './config'
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import userRoute from './routes/userRoute'
+import userRoute from './routes/userRoute';
+import product from './routes/productRoute'
 
 dotenv.config();
 
@@ -23,15 +24,17 @@ app.use(bodyParser.json());
 
 app.use("/api/users", userRoute);
 
-app.get("/api/products/:id/",(req, res) => {
-    const productId = req.params.id;
-    const product = data.products.find(x=>x._id === productId);
-    if(product)
-        res.send(product);
-    else
-        res.status(404).send({msg: "Product Not Found"})
+app.use("/api/products", product);
+
+// app.get("/api/products/:id/",(req, res) => {
+//     const productId = req.params.id;
+//     const product = data.products.find(x=>x._id === productId);
+//     if(product)
+//         res.send(product);
+//     else
+//         res.status(404).send({msg: "Product Not Found"})
     
-});
+// });
 
 app.get("/api/products/",(req, res) => {
     res.send(data.products);
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,44 +1,43 @@
 export default {
-    products: [
-        {
-        _id:'1',
-        name: 'Slim Shirt',
-        category: 'Shirts',
-        image:'/images/d1.jpg',
-        price:60,
-        brand: 'Nike',
-        rating: 4.5,
-        numReviews: 10
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 10
     },
     {
-        _id:'2',
-        name: 'Fit Shirt',
-        category: 'Shirts',
-        image: '/images/d1.jpg',
-        price:85,
-        brand: 'Nike',
-        rating: 4.0,
-        numReviews: 10
+      _id: '2',
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' Nike',
+      rating: 4.2,
+      numReviews: 5
     },
     {
-        _id:'3',
-        name: 'Best Pants',
-        category: 'Pants',
-        image: '/images/d1.jpg',
-        price:40,
-        brand: 'Nike',
-        rating: 4.9,
-        numReviews: 10
+      _id: '3',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    }, {
+      _id: '4',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
     },
-    {
-        _id:'4',
-        name: 'Worst Pants',
-        category: 'Pants',
-        image: '/images/d1.jpg',
-        price:5,
-        brand: 'Crikey',
-        rating: 0.4,
-        numReviews: 16
-    },
-]
+  ]
 }
